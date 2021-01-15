a010d7f8452c2033b4eb036620920c24aa5c4b4b
Bess Campbell <bess.k.campbell@gmail.com>
2021-01-15 13:45:52 -0800

    Add index view/route for flavor

---
+++ Patisserie/Controllers/FlavorsController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Patisserie.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Patisserie.Controllers
+{
+  [Authorize]
+  public class FlavorsController : Controller 
+  {
+    private readonly PatisserieContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    public FlavorsController(UserManager<ApplicationUser> userManager, PatisserieContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public async Task<ActionResult> Index()
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FinndByIdAsync(userId);
+      var userFlavors = _db.Flavors.Where(entry.User.Id == currentUser.Id).ToList();
+      return View(userFlavors);
+    }
+
+  }
+}
